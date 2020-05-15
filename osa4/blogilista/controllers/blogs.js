777472b99e2abce59eaaf06345694c5082864156
Erno <ernovenalainen@hotmail.com>
2020-05-15 08:22:28 +0300

    4.14* blogilistan laajennus, step2

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -30,4 +30,18 @@ blogsRouter.delete('/:id', async (request, response) => {
     response.status(204).end()
 })
 
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        title:  body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    await Blog.findByIdAndUpdate(request.params.id, blog, {new : true})
+    response.json(blog.toJSON)
+})
+
 module.exports = blogsRouter
