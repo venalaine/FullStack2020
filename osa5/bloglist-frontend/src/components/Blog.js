649456e0c66d871d8f9147f09e6d5fd432f80dd7
Erno <ernovenalainen@hotmail.com>
2020-06-24 19:41:11 +0300

    blogilistan frontend, step7

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,39 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showFull, setShowFull] = useState(false)
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  if (showFull === false) {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={() => setShowFull(true)
+        }>View</button>
+      </div>
+    )
+  }
+
+  else {
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={() => setShowFull(false)
+        }>Hide</button>
+        <br />
+        {blog.url}
+        <br />
+        {blog.likes}
+        <br />
+        {blog.user.username}
+      </div>
+    )
+  }
+}
 
 export default Blog
