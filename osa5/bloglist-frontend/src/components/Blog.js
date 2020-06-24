8997e85e690964552e58f0dc3793feb13cfe311e
Erno <ernovenalainen@hotmail.com>
2020-06-24 21:28:34 +0300

    blogilistan frontend, step8

--- osa5/bloglist-frontend/src/components/Blog.js
+++ osa5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,25 @@
 import React, { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({ blog }) => {
   const [showFull, setShowFull] = useState(false)
+  let [likes, setLikes] = useState(blog.likes)
+  
+  const addLikes = () => {
+    const blogToUpdate = {
+      id: blog.id,
+      user: blog.user.id,
+      title: blog.title,
+      author: blog.author,
+      url: blog.url,
+      likes: likes + 1
+    }
+
+    blogService.update(blogToUpdate)
+    
+    setLikes(likes + 1)
+
+  }
 
   const blogStyle = {
     paddingTop: 10,
@@ -28,7 +46,7 @@ const Blog = ({ blog }) => {
         <br />
         {blog.url}
         <br />
-        {blog.likes}
+        {likes} <button onClick={addLikes}> Like </button>
         <br />
         {blog.user.username}
       </div>
--- osa5/bloglist-frontend/src/services/blogs.js
+++ osa5/bloglist-frontend/src/services/blogs.js
@@ -21,4 +21,14 @@ const create = async newObject => {
   return response.data
 }
 
-export default { getAll, setToken, create }
+const update = async objectToUpdate => {
+  
+  const config = {
+    headers: { Authorization: token },
+  }
+
+  const response = await axios.put(baseUrl + `/${objectToUpdate.id}`, objectToUpdate, config)
+  return response.data
+}
+
+export default { getAll, setToken, create, update }
