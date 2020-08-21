ff8c34c6f00da4bb5074196696bffc7ebbd8192c
Erno <ernovenalainen@hotmail.com>
2020-08-21 12:46:31 +0300

    5.14: blogilistan testit, step2

--- osa5/bloglist-frontend/src/components/Blogs.test.js
+++ osa5/bloglist-frontend/src/components/Blogs.test.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('render right content', () => {
+
   const blog = {
     title: 'title of the blog',
     author: 'Antti Author',
     url: 'www.alko.fi',
-    likes: 100
+    likes: 100,
   }
 
   const component = render(
     <Blog blog={blog} />
   )
+
+  component.debug()
 
   expect(component.container).toHaveTextContent(
     'title of the blog',
@@ -27,7 +30,42 @@ test('render right content', () => {
     'www.alko.fi',
   )
 
-  expect(component.container).not.toHaveValue(
+  expect(component.container).not.toHaveTextContent(
     100
   )
+})
+
+test('after buttton click there is more content rendered', () => {
+
+  const user = {
+    id: 1,
+    username: 'Testimies',
+  }
+
+  const blog = {
+    title: 'title of the blog',
+    author: 'Antti Author',
+    url: 'www.example.fi',
+    likes: 100,
+    user: user,
+  }
+
+  const component = render(
+    <Blog blog={blog} user={user} />
+  )
+
+  const button = component.getByText('View')
+  fireEvent.click(button)
+
+  component.debug()
+
+  expect(component.container).toHaveTextContent(
+    'www.example.fi',
+  )
+
+  expect(component.container).toHaveTextContent(
+    '100',
+  )
+
+
 })
