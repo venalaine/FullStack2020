63a0fc59d4d5fc768511c9458c43f66ecb4ec9f1
Erno <ernovenalainen@hotmail.com>
2020-09-14 19:05:15 +0300

    8.19 genren kirjat, osa 1

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,6 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+const RenderBooks = ({ filter, books }) => {
+  if (filter === null || filter === 'all genres') {
+    return (
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              author
+          </th>
+            <th>
+              published
+          </th>
+          </tr>
+          {books.map(b =>
+            <tr key={b.title}>
+              <td>{b.title}</td>
+              <td>{b.author.name}</td>
+              <td>{b.published}</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    )
+  }
+
+  let filteredBooks = books.filter(b => b.genres.includes(filter))
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th></th>
+          <th>
+            author
+      </th>
+          <th>
+            published
+      </th>
+        </tr>
+        {filteredBooks.map(b =>
+          <tr key={b.title}>
+            <td>{b.title}</td>
+            <td>{b.author.name}</td>
+            <td>{b.published}</td>
+          </tr>
+        )}
+      </tbody>
+    </table >
+  )
+}
 
 const Books = ({ show, booksFromDB }) => {
+  const [filter, setFilter] = useState(null)
 
   if (!show) {
     return null
@@ -8,30 +61,17 @@ const Books = ({ show, booksFromDB }) => {
 
   const books = booksFromDB
 
+  let genreSet = new Set()
+  books.map(book => book.genres.map(genre => genreSet.add(genre)))
+  genreSet.add('all genres')
+ 
+
   return (
     <div>
       <h2>books</h2>
-
-      <table>
-        <tbody>
-          <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
-          </tr>
-          {books.map(a =>
-            <tr key={a.title}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+      in genre <b>patterns</b>
+      <RenderBooks filter={filter} books={books}/>
+      {Array.from(genreSet).map(genre => <button key={genre} onClick={() => setFilter(genre)}>{genre}</button>)}
     </div>
   )
 }
--- osa8/library-frontend/src/queries.js
+++ osa8/library-frontend/src/queries.js
@@ -18,6 +18,7 @@ query {
     author  {
       name
     }
+    genres
   }
 }
 `
