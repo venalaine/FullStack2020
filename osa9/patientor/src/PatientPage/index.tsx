4662a6f431210ae516668a499e7829788bf9b983
Erno <ernovenalainen@hotmail.com>
2020-09-28 11:01:31 +0300

    lisatty unohtunut ikoni tehtavaan 9.17: patientor, step2

--- osa9/patientor/src/PatientPage/index.tsx
+++ osa9/patientor/src/PatientPage/index.tsx
@@ -4,35 +4,47 @@ import { useParams } from 'react-router-dom';
 import { Patient } from "../types";
 import { apiBaseUrl } from '../constants';
 import { useStateValue } from '../state';
+import { Icon } from 'semantic-ui-react';
 
 const PatienPage: React.FC = () => {
     const [{ patient }, dispatch] = useStateValue();
 
     const { id } = useParams<{ id: string | undefined }>();
 
-        const fetchPatient = async () => {
-            try {
-                const { data: patientFromApi } = await axios.get<Patient>(
-                    `${apiBaseUrl}/patients/${id}`
-                );
-                dispatch({ type: "SET_SINGLE_PATIENT", payload: patientFromApi });
-            } catch (e) {
-                console.error(e);
-            }
-        };
-
-        if (patient[id] === undefined) {
-            fetchPatient();
-        } else if (patient[id] !== undefined && patient[id].id !== id) {
-            fetchPatient();
+    const fetchPatient = async () => {
+        try {
+            const { data: patientFromApi } = await axios.get<Patient>(
+                `${apiBaseUrl}/patients/${id}`
+            );
+            dispatch({ type: "SET_SINGLE_PATIENT", payload: patientFromApi });
+        } catch (e) {
+            console.error(e);
         }
-        
+    };
+
+    if (patient[id] === undefined) {
+        fetchPatient();
+    } else if (patient[id] !== undefined && patient[id].id !== id) {
+        fetchPatient();
+    }
+
+    const renderIcon = (gender: string) => {
+
+        const iconName = gender === "male" ? "mars" : gender === "female" ? "venus" : "genderless";
+
+        return (
+            <>
+                <Icon name={iconName} size="large" />
+            </>
+        );
+    };
+
     return (
         <>
             {Object.values(patient).map(
                 (p: Patient) =>
                     <div key={p.id}>
-                        <h3>{p.name}</h3>
+                        <h2>{p.name} {renderIcon(p.gender)}</h2>
                         <p>ssn: {p.ssn}</p>
                         <p>occupation: {p.occupation}</p>
                     </div>
